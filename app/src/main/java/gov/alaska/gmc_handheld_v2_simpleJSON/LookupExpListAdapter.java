7c15432541416454336a844f0a0dc8298232db67
Tommy Folan <tommy.folan@alaska.gov>
2020-08-18 15:53:15 -0800

    Testing coloring of specific sections of the display.

--- app/src/main/java/gov/alaska/gmc_handheld_v2_simpleJSON/LookupExpListAdapter.java
+++ app/src/main/java/gov/alaska/gmc_handheld_v2_simpleJSON/LookupExpListAdapter.java
@@ -18,138 +18,146 @@ import java.util.Map;
 
 public class LookupExpListAdapter extends BaseExpandableListAdapter {
 
-    final Context context;
-    final List<String> inventoryLabels;
-    final Map<String, List<SpannableStringBuilder>> inventoryDetailsDict;
+	final Context context;
+	final List<String> inventoryLabels;
+	final Map<String, List<SpannableStringBuilder>> inventoryDetailsDict;
 
-    public LookupExpListAdapter(Context context, List<String> inventoryLabels, Map<String, List<SpannableStringBuilder>> inventoryDetailsDict) {
-        this.context = context;
-        this.inventoryLabels = inventoryLabels;
-        this.inventoryDetailsDict = inventoryDetailsDict;
-    }
+	public LookupExpListAdapter(Context context, List<String> inventoryLabels, Map<String, List<SpannableStringBuilder>> inventoryDetailsDict) {
+		this.context = context;
+		this.inventoryLabels = inventoryLabels;
+		this.inventoryDetailsDict = inventoryDetailsDict;
+	}
 
-    @Override
-    public int getGroupCount() {
-        return inventoryLabels.size();
-    }
+	@Override
+	public int getGroupCount() {
+		return inventoryLabels.size();
+	}
 
-    @Override
-    public int getChildrenCount(int groupPosition) {
-        return inventoryDetailsDict.get(inventoryLabels.get(groupPosition)).size();
-    }
+	@Override
+	public int getChildrenCount(int groupPosition) {
+		return inventoryDetailsDict.get(inventoryLabels.get(groupPosition)).size();
+	}
 
-    @Override
-    public Object getGroup(int groupPosition) {
-        return inventoryLabels.get(groupPosition);
-    }
+	@Override
+	public Object getGroup(int groupPosition) {
+		return inventoryLabels.get(groupPosition);
+	}
 
-    @Override
-    public Object getChild(int groupPosition, int childPosition) {
-        return inventoryDetailsDict.get(inventoryLabels.get(groupPosition)).get(childPosition);
-    }
+	@Override
+	public Object getChild(int groupPosition, int childPosition) {
+		return inventoryDetailsDict.get(inventoryLabels.get(groupPosition)).get(childPosition);
+	}
 
-    @Override
-    public long getGroupId(int groupPosition) {
-        return groupPosition;
-    }
+	@Override
+	public long getGroupId(int groupPosition) {
+		return groupPosition;
+	}
 
-    @Override
-    public long getChildId(int groupPosition, int childPosition) {
-        return childPosition;
-    }
+	@Override
+	public long getChildId(int groupPosition, int childPosition) {
+		return childPosition;
+	}
 
-    @Override
-    public boolean hasStableIds() {
-        return false;
-    }
+	@Override
+	public boolean hasStableIds() {
+		return false;
+	}
 
-    @Override
-    public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
+	@Override
+	public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
 
-        String lang = (String) getGroup(groupPosition);
+		String lang = (String) getGroup(groupPosition);
 
-        if (convertView == null) {
-            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            convertView = inflater.inflate(R.layout.exp_list_parent, null);
-        }
+		if (convertView == null) {
+			LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+			convertView = inflater.inflate(R.layout.exp_list_parent, null);
+		}
 
-        TextView txtParent = convertView.findViewById(R.id.txtParent);
+		TextView txtParent = convertView.findViewById(R.id.txtParent);
 
-        if (inventoryDetailsDict.toString().contains("Wells")) {
-            txtParent.setBackgroundColor(Color.parseColor("#ff92cbff"));
-        } else if (inventoryDetailsDict.toString().contains("Boreholes") | inventoryDetailsDict.toString().contains("Prospect")) {
-            txtParent.setBackgroundColor(Color.parseColor("#ffcddfce"));
-        } else if (inventoryDetailsDict.toString().contains("Outcrops")) {
-            txtParent.setBackgroundColor(Color.parseColor("#ffffffb4"));
-        } else if (inventoryDetailsDict.toString().contains("Shotpoints")) {
-            txtParent.setBackgroundColor(Color.parseColor("#ffff8a86"));
-        } else {
-            txtParent.setBackgroundColor(Color.TRANSPARENT);
-        }
+		if (inventoryDetailsDict.toString().contains("Wells")) {
+			txtParent.setBackgroundColor(Color.parseColor("#ff92cbff"));
+		} else if (inventoryDetailsDict.toString().contains("Boreholes") | inventoryDetailsDict.toString().contains("Prospect")) {
+			txtParent.setBackgroundColor(Color.parseColor("#ffcddfce"));
+		} else if (inventoryDetailsDict.toString().contains("Outcrops")) {
+			txtParent.setBackgroundColor(Color.parseColor("#ffffffb4"));
+		} else if (inventoryDetailsDict.toString().contains("Shotpoints")) {
+			txtParent.setBackgroundColor(Color.parseColor("#ffff8a86"));
+		} else {
+			txtParent.setBackgroundColor(Color.TRANSPARENT);
+		}
 
-        txtParent.setText(lang);
+		txtParent.setText(lang);
 
 
-        return convertView;
+		return convertView;
 
-    }
+	}
 
-    @Override
-    public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
-        SpannableStringBuilder topic = (SpannableStringBuilder) getChild(groupPosition, childPosition);
+	@Override
+	public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
+		SpannableStringBuilder topic = (SpannableStringBuilder) getChild(groupPosition, childPosition);
 
-        if (convertView == null) {
-            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            convertView = inflater.inflate(R.layout.exp_list_child, null);
-        }
+		if (convertView == null) {
+			LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+			convertView = inflater.inflate(R.layout.exp_list_child, null);
+		}
 
-        TextView txtChild = convertView.findViewById(R.id.txtChild);
+		TextView txtChild = convertView.findViewById(R.id.txtChild);
 
-        if (childPosition % 2 != 0) {
-            txtChild.setBackgroundColor(Color.parseColor("#ffd1d4d6"));
-        } else {
-            txtChild.setBackgroundColor(Color.parseColor("#ffdee2e6"));
-        }
 
-//        if (topic.toString().contains("Boreholes") || topic.toString().contains("Outcrops") || topic.toString().contains("Shotpoints") || topic.toString().contains("Wells") ) {
-//            topic.setSpan(new ForegroundColorSpan(Color.RED), 0, topic.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
-//            topic.setSpan(new BackgroundColorSpan(Color.RED), 0, topic.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
-//            if(childPosition % 2 != 0) {
-//                txtChild.setBackgroundColor(Color.parseColor("#fff1f4f7"));
-//            }
-//            else{
-//                txtChild.setBackgroundColor(Color.parseColor("#ffd1d4d6"));
-//            }
-//        } else {
-//            txtChild.setBackgroundColor(Color.TRANSPARENT);
-//        }
+		//Testing the code below depends on Chris.
 
-        int width = getScreenWidth();
+		if (getChild(groupPosition, childPosition).toString().contains("Well")) {
+			if (childPosition % 2 != 0) {
+				txtChild.setBackgroundColor(Color.parseColor("#c892cbff"));
+			} else {
+				txtChild.setBackgroundColor(Color.parseColor("#6492cbff"));
+			}
+		} else if (inventoryDetailsDict.toString().contains("Boreholes") | inventoryDetailsDict.toString().contains("Prospect")) {
+            if (childPosition % 2 != 0) {
+                txtChild.setBackgroundColor(Color.parseColor("#c8cddfce"));
+            }else{
+                txtChild.setBackgroundColor(Color.parseColor("#64cddfce"));
+            }
+		} else if (inventoryDetailsDict.toString().contains("Outcrops")) {
+			txtChild.setBackgroundColor(Color.parseColor("#ffffffb4"));
+		} else if (inventoryDetailsDict.toString().contains("Shotpoints")) {
+            if (childPosition % 2 != 0) {
+                txtChild.setBackgroundColor(Color.parseColor("#7dff8a86"));
+            }else{
+                txtChild.setBackgroundColor(Color.parseColor("#32ff8a86"));
+            }
+		} else {
+			txtChild.setBackgroundColor(Color.TRANSPARENT);
+		}
+
+		int width = getScreenWidth();
 //        System.out.println("Width: " + width );
 
-        if (topic.length() > (80)) {
-            txtChild.setText(createIndentedText(topic, 0, 30));
-        } else {
-            txtChild.setText(topic);
-        }
-        return convertView;
-    }
+		if (topic.length() > (80)) {
+			txtChild.setText(createIndentedText(topic, 0, 30));
+		} else {
+			txtChild.setText(topic);
+		}
+		return convertView;
+	}
 
-    @Override
-    public boolean isChildSelectable(int groupPosition, int childPosition) {
-        return true;
-    }
+	@Override
+	public boolean isChildSelectable(int groupPosition, int childPosition) {
+		return true;
+	}
 
 
-    public static SpannableString createIndentedText(Spannable text, int marginFirstLine, int marginNextLines) {
-        //https://www.programmersought.com/article/45371641877/
-        SpannableString result = new SpannableString(text);
-        result.setSpan(new LeadingMarginSpan.Standard(marginFirstLine, marginNextLines), 0, text.length(), 0);
-        return result;
-    }
+	public static SpannableString createIndentedText(Spannable text, int marginFirstLine, int marginNextLines) {
+		//https://www.programmersought.com/article/45371641877/
+		SpannableString result = new SpannableString(text);
+		result.setSpan(new LeadingMarginSpan.Standard(marginFirstLine, marginNextLines), 0, text.length(), 0);
+		return result;
+	}
 
-    public static int getScreenWidth() {
-        //https://stackoverflow.com/a/31377616
-        return Resources.getSystem().getDisplayMetrics().widthPixels;
-    }
+	public static int getScreenWidth() {
+		//https://stackoverflow.com/a/31377616
+		return Resources.getSystem().getDisplayMetrics().widthPixels;
+	}
 }
