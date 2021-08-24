4a41f7d85a9ee190645e40a490156ac52d3309ab
Tommy Folan <tommy.folan@alaska.gov>
2021-08-24 14:40:55 -0800

    Remove newlines from AddContainer.  Convert onClick functions to lambda in AddContainer

--- app/src/main/java/gov/alaska/gmchandheld/AddContainer.java
+++ app/src/main/java/gov/alaska/gmchandheld/AddContainer.java
@@ -1,8 +1,6 @@
 package gov.alaska.gmchandheld;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -12,7 +10,6 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import androidx.annotation.Nullable;
-import androidx.appcompat.widget.Toolbar;
 import com.google.android.gms.common.api.CommonStatusCodes;
 import com.google.android.gms.vision.barcode.Barcode;
 import com.google.zxing.integration.android.IntentIntegrator;
@@ -36,17 +33,16 @@ public class AddContainer extends BaseActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-
 		addContainerBarcodeET = findViewById(R.id.barcodeET);
-		final EditText addContainerNameET = findViewById(R.id.nameET);
-		final EditText addContainerRemarkET = findViewById(R.id.remarkET);
-		final Button submit_button = findViewById(R.id.submitBtn);
+		EditText addContainerNameET = findViewById(R.id.nameET);
+		EditText addContainerRemarkET = findViewById(R.id.remarkET);
+		Button submit_button = findViewById(R.id.submitBtn);
 		final RemoteApiUIHandler remoteApiUIHandler = new RemoteApiUIHandler();
-//		SharedPreferences sp = getSharedPreferences(Configuration.SHARED_PREFS, MODE_PRIVATE);
 		boolean cameraOn = (sp.getBoolean("cameraOn", false));
 		Button cameraBtn = findViewById(R.id.cameraBtn);
 		if(!cameraOn){
-			LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(0, LinearLayout.LayoutParams.WRAP_CONTENT);
+			LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(0,
+					LinearLayout.LayoutParams.WRAP_CONTENT);
 			params.weight = 7.75f;
 			addContainerBarcodeET.setLayoutParams(params);
 			addContainerNameET.setLayoutParams(params);
@@ -56,33 +52,29 @@ public class AddContainer extends BaseActivity {
 			qrScan = new IntentIntegrator(this);
 			qrScan.setBeepEnabled(true);
 		}
-		cameraBtn.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View view) {
-				if (Build.VERSION.SDK_INT <= 24) {
-					qrScan.initiateScan();
-				} else {
-					Intent intent = new Intent(AddContainer.this, CameraToScanner.class);
-					startActivityForResult(intent, 0);
-				}
+		cameraBtn.setOnClickListener(view -> {
+			if (Build.VERSION.SDK_INT <= 24) {
+				qrScan.initiateScan();
+			} else {
+				Intent intent = new Intent(AddContainer.this, CameraToScanner.class);
+				startActivityForResult(intent, 0);
 			}
 		});
 		// KeyListener listens if enter is pressed
-		addContainerBarcodeET.setOnKeyListener(new View.OnKeyListener() {
-			public boolean onKey(View v, int keyCode, KeyEvent event) {
-				// if "enter" is pressed
-				if ((event.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)) {
-					addContainerNameET.requestFocus();
-					return true;
-				}
-				return false;
+		addContainerBarcodeET.setOnKeyListener((v, keyCode, event) -> {
+			// if "enter" is pressed
+			if ((event.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)){
+				addContainerNameET.requestFocus();
+				return true;
 			}
+			return false;
 		});
 		// KeyListener listens if enter is pressed
 		addContainerNameET.setOnKeyListener(new View.OnKeyListener() {
 			public boolean onKey(View v, int keyCode, KeyEvent event) {
 				// if "enter" is pressed
-				if ((event.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)) {
+				if ((event.getAction() == KeyEvent.ACTION_DOWN) &&
+						(keyCode == KeyEvent.KEYCODE_ENTER)){
 					addContainerRemarkET.requestFocus();
 					return true;
 				}
@@ -100,11 +92,15 @@ public class AddContainer extends BaseActivity {
 							String container = addContainerBarcodeET.getText().toString();
 							if (!container.isEmpty()) {
 								RemoteApiUIHandler remoteApiUIHandler = new RemoteApiUIHandler();
-								RemoteApiUIHandler.setUrlFirstParameter(addContainerBarcodeET.getText().toString());
-								RemoteApiUIHandler.setAddContainerName(addContainerNameET.getText().toString());
-								RemoteApiUIHandler.setAddContainerRemark(addContainerRemarkET.getText().toString());
+								RemoteApiUIHandler.setUrlFirstParameter(
+										addContainerBarcodeET.getText().toString());
+								RemoteApiUIHandler.setAddContainerName(
+										addContainerNameET.getText().toString());
+								RemoteApiUIHandler.setAddContainerRemark(
+										addContainerRemarkET.getText().toString());
 								remoteApiUIHandler.setDownloading(true);
-								new RemoteApiUIHandler.ProcessDataForDisplay(AddContainer.this).execute();
+								new RemoteApiUIHandler.ProcessDataForDisplay(
+										AddContainer.this).execute();
 							}
 							addContainerBarcodeET.setText("");
 							addContainerNameET.setText("");
