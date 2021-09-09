4c5b612f2610571baf9a2fab9677f0abff2572dc
Tommy Folan <tommy.folan@alaska.gov>
2021-09-09 08:20:11 -0800

    Update Quality to work with the ExecutiveService.

--- app/src/main/java/gov/alaska/gmchandheld/Quality.java
+++ app/src/main/java/gov/alaska/gmchandheld/Quality.java
@@ -12,29 +12,37 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 import com.google.android.gms.common.api.CommonStatusCodes;
 import com.google.android.gms.vision.barcode.Barcode;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class Quality extends BaseActivity implements IssuesFragment.onMultiChoiceListener {
     private EditText barcodeET;
     private TextView showIssuesTV;
-    private static ArrayList<String> selectedItems;
+    private static ArrayList < String > selectedItems;
     private static boolean[] checkedItems;
-    private static ArrayList<String> selectedItemsDisplayList;
+    private static ArrayList < String > selectedItemsDisplayList;
+    private String data;
 
     public Quality() {
-        selectedItems = new ArrayList<>();
+        selectedItems = new ArrayList < > ();
         selectedItems.add("needs_inventory");
-        selectedItemsDisplayList = new ArrayList<>();
+        selectedItemsDisplayList = new ArrayList < > ();
         selectedItemsDisplayList.add("Needs Inventory");
         checkedItems = new boolean[10];
         checkedItems[0] = true;
     }
 
-    public static ArrayList<String> getSelectedItems() {
+    public static ArrayList < String > getSelectedItems() {
         return selectedItems;
     }
 
@@ -42,7 +50,7 @@ public class Quality extends BaseActivity implements IssuesFragment.onMultiChoic
         return checkedItems;
     }
 
-    public static ArrayList<String> getSelectedItemsDisplayList() {
+    public static ArrayList < String > getSelectedItemsDisplayList() {
         return selectedItemsDisplayList;
     }
 
@@ -64,11 +72,11 @@ public class Quality extends BaseActivity implements IssuesFragment.onMultiChoic
         barcodeET = findViewById(R.id.barcodeET);
         final EditText remarkET = findViewById(R.id.remarkET);
         showIssuesTV = findViewById(R.id.showIssuesTV);
-        if (!selectedItemsDisplayList.isEmpty()){
+        if (!selectedItemsDisplayList.isEmpty()) {
             showIssuesTV.setText(listToString(selectedItemsDisplayList));
         }
         Button cameraBtn = findViewById(R.id.cameraBtn);
-        if (!sp.getBoolean("cameraOn", false)){
+        if (!sp.getBoolean("cameraOn", false)) {
             LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(0,
                     LinearLayout.LayoutParams.WRAP_CONTENT);
             params.weight = 7.75f;
@@ -95,13 +103,72 @@ public class Quality extends BaseActivity implements IssuesFragment.onMultiChoic
             }
             return false;
         });
-        if (!RemoteApiUIHandler.isDownloading()) {
+        if (!downloading) {
+            downloading = true;
             // onClickListener listens if the submit button is clicked
             findViewById(R.id.submitBtn).setOnClickListener(v -> {
                 if (!(TextUtils.isEmpty(barcodeET.getText()))) {
                     if (!barcodeET.getText().toString().isEmpty()) {
-                        new RemoteApiUIHandler(this, barcodeET.getText().toString(),
-                                remarkET.getText().toString(), selectedItems).execute();
+                        String barcode = null;
+                        String remark = null;
+                        try {
+                            barcode = URLEncoder.encode(barcodeET.getText().toString(), "utf-8");
+                            if (remarkET.getText() != null) {
+                                remark = URLEncoder.encode(remarkET.getText().toString(), "utf-8");
+                            }
+                        } catch (UnsupportedEncodingException e) {
+                            //                            exception = new Exception(e.getMessage());
+                        }
+                        StringBuilder sb = new StringBuilder();
+                        if (barcode != null) {
+                            sb.append("barcode=").append(barcode);
+                        }
+                        if (remark != null) {
+                            sb.append("&remark=").append(remark);
+                        }
+                        if (selectedItems != null) {
+                            sb.append(containersToUrlList(selectedItems, "i"));
+                        }
+                        String url = baseURL + "addinventoryquality.json?" + sb.toString();
+                        Runnable runnable = new Runnable() {
+                            @Override
+                            public void run() {
+                                if (thread.isInterrupted()) {
+                                    return;
+                                }
+                                final ExecutorService service =
+                                        Executors.newFixedThreadPool(1);
+                                final Future < String > task =
+                                        service.submit(new NewRemoteAPIDownload(url));
+                                try {
+                                    data = task.get();
+                                } catch (ExecutionException e) {
+                                    e.printStackTrace();
+                                } catch (InterruptedException e) {
+                                    e.printStackTrace();
+                                    return;
+                                }
+                                runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        if (null == data) {
+                                            Toast.makeText(Quality.this,
+                                                    "There was a problem.  " +
+                                                            "The inventory was not added.",
+                                                    Toast.LENGTH_SHORT).show();
+                                            barcodeET.requestFocus();
+                                        } else if (data.contains("success")) {
+                                            Toast.makeText(Quality.this,
+                                                    "The inventory was added.",
+                                                    Toast.LENGTH_SHORT).show();
+                                            barcodeET.requestFocus();
+                                        }
+                                    }
+                                });
+                            }
+                        };
+                        thread = new Thread(runnable);
+                        thread.start();
                     }
                     barcodeET.setText("");
                     remarkET.setText("");
@@ -116,6 +183,7 @@ public class Quality extends BaseActivity implements IssuesFragment.onMultiChoic
                     showIssuesTV.setText(listToString(selectedItemsDisplayList));
                 }
             });
+            downloading = false;
         }
         showIssuesTV = findViewById(R.id.showIssuesTV);
         Button issuesBtn = findViewById(R.id.issuesBtn);
@@ -149,19 +217,39 @@ public class Quality extends BaseActivity implements IssuesFragment.onMultiChoic
     }
 
     @Override
-    public void onPostitiveButtonClicked(String[] list, ArrayList<String> selectedItems) {
+    public void onPostitiveButtonClicked(String[] list, ArrayList < String > selectedItems) {
         showIssuesTV.setText(listToString(selectedItemsDisplayList));
     }
 
     @Override
-    public void onNegativebuttonClicked() { }
+    public void onNegativebuttonClicked() {}
 
-    public String listToString(ArrayList<String> arrList){
+    public String listToString(ArrayList < String > arrList) {
         StringBuilder sb = new StringBuilder();
         //used to display the list in the app
         sb.setLength(0); //clears the display list so unchecked items are removed
-        for (String s : arrList) {
+        for (String s: arrList) {
             sb.append(s).append("\n");
+        }
+        return sb.toString();
+    }
+
+    public String containersToUrlList(ArrayList < String > list, String paramKeyword) {
+        String delim = "&" + paramKeyword + "=";
+        StringBuilder sb = new StringBuilder();
+        if (list != null && list.size() > 0) {
+            sb.append(delim);
+            int i = 0;
+            while (i < list.size() - 1) {
+                try {
+                    sb.append(URLEncoder.encode(list.get(i), "utf-8"));
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                }
+                sb.append(delim);
+                i++;
+            }
+            sb.append(list.get(i));
         }
         return sb.toString();
     }
