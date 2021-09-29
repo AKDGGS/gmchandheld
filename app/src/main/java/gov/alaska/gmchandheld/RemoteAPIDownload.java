1c5f473700fb9996533fc1ccf551a64fe6281457
Tommy Folan <tommy.folan@alaska.gov>
2021-09-29 12:57:04 -0800

    Added thread safe code to RemoteAPIDownload.

--- app/src/main/java/gov/alaska/gmchandheld/RemoteAPIDownload.java
+++ app/src/main/java/gov/alaska/gmchandheld/RemoteAPIDownload.java
@@ -1,42 +1,69 @@
 package gov.alaska.gmchandheld;
 
-import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.ProtocolException;
 import java.net.URL;
 
 public class RemoteAPIDownload implements Runnable {
-    private String url;
-    private RemoteAPIDownloadCallback downloadingCallback;
 
-    public void setAPICallback(RemoteAPIDownloadCallback downloadingCallback){
-        this.downloadingCallback = downloadingCallback;
-    }
 
-    public void setUrl(String url) {
-        this.url = url;
-    }
+    private RemoteAPIDownloadCallback remoteAPIDownloadCallback;
+    private String url, token;
 
-    @Override
-    public void run() {
-        InputStream inputStream;
-        HttpURLConnection connection;
-        try {
-            URL myURL = new URL(url);
-            connection = (HttpURLConnection) myURL.openConnection();
-            connection.setRequestMethod("GET");
-            connection.setRequestProperty("Authorization", "Token " + BaseActivity.apiKeyBase);
-            connection.setReadTimeout(10 * 1000);
-            connection.setConnectTimeout(5 * 1000);
-            connection.connect();
-            try {
-                inputStream = connection.getInputStream();
-            } catch (Exception e) {
-                inputStream = connection.getErrorStream();
+    public void setFetchDataObj(String url, String token, RemoteAPIDownloadCallback remoteAPIDownloadCallback) throws Exception {
+        if (url == null) {
+            throw new Exception("URL is null");
+        }
+        if (remoteAPIDownloadCallback == null) {
+            throw new Exception("The callback can't be null");
+        }
+
+        new URL(url);
+
+        synchronized (this) {
+            if (this.url != null) {
+                throw new Exception("Fetch is busy");
             }
+            this.url = url;
+            this.token = token;
+            this.remoteAPIDownloadCallback = remoteAPIDownloadCallback;
+            notify();
+        }
+    }
+
+    public void run() {
+        //Infinitely produce items
+        while (true) {
+            synchronized (this) {
+                try {
+                    wait();
+                } catch (Exception e) {
+//                    dataCallback.displayException(e);
+                    continue;
+                }
+            }
+
             try {
+                InputStream inputStream;
+                HttpURLConnection connection;
+                URL myURL;
+                synchronized (this) {
+                    myURL = new URL(url);
+                }
+                connection = (HttpURLConnection) myURL.openConnection();
+                connection.setRequestMethod("GET");
+                if (token != null) {
+                    connection.setRequestProperty("Authorization", "Token " + token);
+                }
+                connection.setReadTimeout(10 * 1000);
+                connection.setConnectTimeout(5 * 1000);
+                connection.connect();
+
+                try {
+                    inputStream = connection.getInputStream();
+                } catch (Exception e) {
+                    inputStream = connection.getErrorStream();
+                }
                 StringBuilder sb = new StringBuilder();
                 byte[] buffer = new byte[4096];
                 int buffer_read = 0;
@@ -48,22 +75,19 @@ public class RemoteAPIDownload implements Runnable {
                 }
                 if (connection.getErrorStream() != null) {
                     sb.append(connection.getResponseMessage());
-                    System.out.println("RCL : " + connection.getResponseCode());
-                    System.out.println("ML " + connection.getResponseMessage());
                 }
-                downloadingCallback.displayData(sb.toString(), connection.getResponseCode(), connection.getResponseMessage());
-
+                remoteAPIDownloadCallback.displayData(sb.toString(), connection.getResponseCode(),
+                        connection.getResponseMessage());
+                inputStream.close();
+                connection.disconnect();
+                synchronized (this) {
+                    remoteAPIDownloadCallback = null;
+                    token = null;
+                    url = null;
+                }
             } catch (Exception e) {
-                e.printStackTrace();
+                remoteAPIDownloadCallback.displayException(e);
             }
-            inputStream.close();
-            connection.disconnect();
-        } catch (ProtocolException e) {
-            e.printStackTrace();
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
         }
     }
 }
