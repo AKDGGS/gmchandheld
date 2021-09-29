60f0b03fd67fe06f597ee843e82e15b70c0eb754
Tommy Folan <tommy.folan@alaska.gov>
2021-09-29 12:57:24 -0800

    Added displayException to RemoteAPIDownloadCallback.

--- app/src/main/java/gov/alaska/gmchandheld/RemoteAPIDownloadCallback.java
+++ app/src/main/java/gov/alaska/gmchandheld/RemoteAPIDownloadCallback.java
@@ -2,4 +2,5 @@ package gov.alaska.gmchandheld;
 
 public interface RemoteAPIDownloadCallback {
     void displayData(String data, int responseCode, String responseMessage);
+    void displayException(Exception e);
 }
