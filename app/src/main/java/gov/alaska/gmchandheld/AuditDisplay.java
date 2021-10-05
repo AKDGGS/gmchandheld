f8718b2d79dabb119af29b65c1f976e8dbeb8e5f
Tommy Folan <tommy.folan@alaska.gov>
2021-10-05 07:40:44 -0800

    Updated clearing behavior when action is successful.

--- app/src/main/java/gov/alaska/gmchandheld/AuditDisplay.java
+++ app/src/main/java/gov/alaska/gmchandheld/AuditDisplay.java
@@ -26,13 +26,13 @@ import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.util.ArrayList;
 
-public class AuditDisplay extends BaseActivity implements RemoteAPIDownloadCallback{
-    private ArrayList < String > containerList;
-    private ArrayAdapter < String > adapter;
-    private EditText auditRemarkET, auditItemET;
+public class AuditDisplay extends BaseActivity implements RemoteAPIDownloadCallback {
+    private ArrayList<String> containerList;
+    private ArrayAdapter<String> adapter;
+    private EditText remarkET, itemET;
     private Button clearAllBtn;
     private int clicks; //used to count double clicks for deletion
-    private TextView auditCountTV;
+    private TextView countTV;
 
     public AuditDisplay() {
         clicks = 0;
@@ -53,24 +53,24 @@ public class AuditDisplay extends BaseActivity implements RemoteAPIDownloadCallb
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         checkAPIkeyExists(this);
-        auditItemET = findViewById(R.id.itemET);
-        auditRemarkET = findViewById(R.id.remarkET);
-        auditCountTV = findViewById(R.id.auditCountTV);
+        itemET = findViewById(R.id.itemET);
+        remarkET = findViewById(R.id.remarkET);
+        countTV = findViewById(R.id.auditCountTV);
         clearAllBtn = findViewById(R.id.clearAllBtn);
         ListView auditContainerListLV = findViewById(R.id.listViewGetContainersToAudit);
-        adapter = new ArrayAdapter < > (this, android.R.layout.simple_list_item_1);
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1);
         auditContainerListLV.setAdapter(adapter);
         containerList = AuditDisplayObjInstance.getInstance().getAuditList();
         adapter.addAll(containerList);
-        auditCountTV.setText(String.valueOf(containerList.size()));
+        countTV.setText(String.valueOf(containerList.size()));
         Button remarkCameraBtn = findViewById(R.id.cameraBtn);
         Button itemCameraBtn = findViewById(R.id.itemCameraBtn);
         if (!sp.getBoolean("cameraOn", false)) {
             LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(0,
                     LinearLayout.LayoutParams.WRAP_CONTENT);
             params.weight = 6.25f;
-            auditRemarkET.setLayoutParams(params);
-            auditItemET.setLayoutParams(params);
+            remarkET.setLayoutParams(params);
+            itemET.setLayoutParams(params);
             remarkCameraBtn.setVisibility(View.GONE);
             itemCameraBtn.setVisibility(View.GONE);
         } else {
@@ -99,102 +99,97 @@ public class AuditDisplay extends BaseActivity implements RemoteAPIDownloadCallb
         });
         final Button addBtn = findViewById(R.id.addContainerBtn);
         addBtn.setOnClickListener(v -> {
-            String container = auditItemET.getText().toString();
+            String container = itemET.getText().toString();
             if (!containerList.contains(container) && !container.isEmpty()) {
                 containerList.add(0, container);
                 adapter.insert(container, 0);
                 adapter.notifyDataSetChanged();
-                auditCountTV.setText(String.valueOf(containerList.size()));
+                countTV.setText(String.valueOf(containerList.size()));
             }
-            auditItemET.setText("");
-            auditItemET.requestFocus();
+            itemET.setText("");
+            itemET.requestFocus();
         });
         clearAllBtn.setOnClickListener(v -> {
-            auditItemET.setText("");
-            auditItemET.requestFocus();
+            itemET.setText("");
+            itemET.requestFocus();
             containerList.clear();
             adapter.clear();
             adapter.notifyDataSetChanged();
-            auditCountTV.setText(String.valueOf(containerList.size()));
+            countTV.setText(String.valueOf(containerList.size()));
         });
-        if (!downloading) {
-            downloading = true;
-            //double click to remove elements
-            auditContainerListLV.setOnItemClickListener((adapterView, view, position, l) -> {
-                clicks++;
-                Handler handler = new Handler();
-                handler.postDelayed(() -> {
-                    if (clicks == 2) {
-                        adapter.remove(containerList.get(position));
-                        containerList.remove(position);
-                        adapter.notifyDataSetChanged();
-                        auditCountTV.setText(String.valueOf(containerList.size()));
-                    }
-                    clicks = 0;
-                }, 500);
-            });
-            // KeyListener listens if enter is pressed
-            auditItemET.setOnKeyListener((v, keyCode, event) -> {
+
+        //double click to remove elements
+        auditContainerListLV.setOnItemClickListener((adapterView, view, position, l) -> {
+            clicks++;
+            Handler handler = new Handler();
+            handler.postDelayed(() -> {
+                if (clicks == 2) {
+                    adapter.remove(containerList.get(position));
+                    containerList.remove(position);
+                    adapter.notifyDataSetChanged();
+                    countTV.setText(String.valueOf(containerList.size()));
+                }
+                clicks = 0;
+            }, 500);
+        });
+        // KeyListener listens if enter is pressed
+        itemET.setOnKeyListener((v, keyCode, event) -> {
+            if ((event.getAction() == KeyEvent.ACTION_UP) &&
+                    (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                addBtn.performClick();
+                itemET.requestFocus();
+                return true;
+            }
+            return false;
+        });
+        // KeyListener listens if enter is pressed
+        remarkET.setOnKeyListener((v, keyCode, event) -> {
+            if (!(TextUtils.isEmpty(remarkET.getText()))) {
                 if ((event.getAction() == KeyEvent.ACTION_UP) &&
                         (keyCode == KeyEvent.KEYCODE_ENTER)) {
-                    addBtn.performClick();
-                    auditItemET.requestFocus();
+                    itemET.requestFocus();
                     return true;
                 }
-                return false;
-            });
-            // KeyListener listens if enter is pressed
-            auditRemarkET.setOnKeyListener((v, keyCode, event) -> {
-                if (!(TextUtils.isEmpty(auditRemarkET.getText()))) {
-                    if ((event.getAction() == KeyEvent.ACTION_UP) &&
-                            (keyCode == KeyEvent.KEYCODE_ENTER)) {
-                        auditItemET.requestFocus();
-                        return true;
-                    }
-                }
-                return false;
-            });
-            // onClickListener listens if the submit button is clicked
-            findViewById(R.id.submitBtn).setOnClickListener(v -> {
-                String remark = null;
-                try {
-                    remark = URLEncoder.encode(auditRemarkET.getText().toString(), "utf-8");
-                } catch (UnsupportedEncodingException e) {
-                    //                            exception = new Exception(e.getMessage());
-                }
+            }
+            return false;
+        });
+        // onClickListener listens if the submit button is clicked
+        findViewById(R.id.submitBtn).setOnClickListener(v -> {
+            String remark = null;
+            try {
+                remark = URLEncoder.encode(remarkET.getText().toString(), "utf-8");
+            } catch (UnsupportedEncodingException e) {
+                //                            exception = new Exception(e.getMessage());
+            }
 
-                try {
-                    remoteAPIDownload.setFetchDataObj(baseURL + "audit.json?remark=" + remark +
-                                    createListForURL(containerList, "c"),
-                            BaseActivity.apiKeyBase,
-                            this);
-                } catch (Exception e) {
-                    System.out.println("Exception: " + e.getMessage());
-                }
-                auditItemET.setText("");
-                auditRemarkET.setText("");
-                auditCountTV.setText("");
-            });
-            downloading = false;
-        }
+            try {
+                remoteAPIDownload.setFetchDataObj(baseURL + "audit.json?remark=" + remark +
+                                createListForURL(containerList, "c"),
+                        BaseActivity.apiKeyBase,
+                        this);
+            } catch (Exception e) {
+                System.out.println("Exception: " + e.getMessage());
+            }
+        });
     }
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         if (Build.VERSION.SDK_INT <= 24) {
             switch (requestCode) {
                 case 1: {
-                    auditRemarkET = findViewById(R.id.remarkET);
+                    remarkET = findViewById(R.id.remarkET);
                     IntentResult result = IntentIntegrator.parseActivityResult(
                             IntentIntegrator.REQUEST_CODE, resultCode, data);
-                    auditRemarkET.setText(result.getContents());
+                    remarkET.setText(result.getContents());
                 }
                 break;
                 case 2: {
-                    auditItemET = findViewById(R.id.itemET);
+                    itemET = findViewById(R.id.itemET);
                     IntentResult result = IntentIntegrator.parseActivityResult(
                             IntentIntegrator.REQUEST_CODE, resultCode, data);
-                    auditItemET.setText(result.getContents());
+                    itemET.setText(result.getContents());
                 }
                 break;
             }
@@ -243,15 +238,17 @@ public class AuditDisplay extends BaseActivity implements RemoteAPIDownloadCallb
                 Toast.makeText(AuditDisplay.this,
                         "There was a problem. The audit was not added.",
                         Toast.LENGTH_SHORT).show();
-                auditRemarkET.requestFocus();
+                remarkET.requestFocus();
             } else if (data.contains("success")) {
-                Toast.makeText(AuditDisplay.this,"The audit was added.",
+                Toast.makeText(AuditDisplay.this, "The audit was added.",
                         Toast.LENGTH_SHORT).show();
-                auditRemarkET.requestFocus();
+                remarkET.requestFocus();
+                remarkET.setText("");
+                itemET.setText("");
                 containerList.clear();
                 adapter.clear();
                 adapter.notifyDataSetChanged();
-                auditCountTV.setText(String.valueOf(containerList.size()));
+                countTV.setText(String.valueOf(containerList.size()));
             }
         });
     }
@@ -263,7 +260,7 @@ public class AuditDisplay extends BaseActivity implements RemoteAPIDownloadCallb
                 @Override
                 public void run() {
                     Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_LONG).show();
-                    System.out.println(e.getMessage());
+                    remarkET.requestFocus();
                 }
             });
         }
