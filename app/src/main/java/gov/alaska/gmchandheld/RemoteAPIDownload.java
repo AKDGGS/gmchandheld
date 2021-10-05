86dd7709c3895a37418dd48a8efb96082c3d8d35
Tommy Folan <tommy.folan@alaska.gov>
2021-10-05 11:40:00 -0800

    Update RemoteAPIDownload so that it handles both Posts and Gets.

--- app/src/main/java/gov/alaska/gmchandheld/RemoteAPIDownload.java
+++ app/src/main/java/gov/alaska/gmchandheld/RemoteAPIDownload.java
@@ -1,29 +1,47 @@
 package gov.alaska.gmchandheld;
 
+import java.io.BufferedReader;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
-public class RemoteAPIDownload implements Runnable {
-    private RemoteAPIDownloadCallback remoteAPIDownloadCallback;
-    private String url, token;
-    private Object lockObj = new Object();
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
 
-    public void setFetchDataObj(String url, String token, RemoteAPIDownloadCallback remoteAPIDownloadCallback) throws Exception {
+public class RemoteAPIDownload implements Runnable {
+
+    private final Object lockObj = new Object();
+    private RemoteAPIDownloadCallback remoteAPIDownloadCallback;
+    private String url, token, barcode, description;
+    private RequestBody body;
+
+    public void setFetchDataObj(String url,
+                                  String token,
+                                  RequestBody body,
+                                  RemoteAPIDownloadCallback remoteAPIDownloadCallback) throws Exception {
         if (url == null) {
             throw new Exception("URL is null");
         }
+        new URL(url);
+
+        if (token == null) {
+            throw new Exception("The token can't be null");
+        }
+
         if (remoteAPIDownloadCallback == null) {
             throw new Exception("The callback can't be null");
         }
 
-        new URL(url);
         synchronized (lockObj) {
             if (this.url != null) {
-                throw new Exception("Fetch is busy");
+                throw new Exception("Uploading is busy");
             }
+
             this.url = url;
             this.token = token;
+            this.body = body;
             this.remoteAPIDownloadCallback = remoteAPIDownloadCallback;
             lockObj.notify();
         }
@@ -36,47 +54,95 @@ public class RemoteAPIDownload implements Runnable {
                 try {
                     lockObj.wait();
                 } catch (Exception e) {
-//                    dataCallback.displayException(e);
+//                    fetchDataCallback.displayException(e);
                     continue;
                 }
             }
 
             try {
-                InputStream inputStream;
-                HttpURLConnection connection;
-                URL myURL;
-                synchronized (lockObj) {
-                    myURL = new URL(url);
-                }
-                connection = (HttpURLConnection) myURL.openConnection();
-                connection.setRequestMethod("GET");
-                if (token != null) {
-                    connection.setRequestProperty("Authorization", "Token " + token);
-                }
-                connection.setReadTimeout(10 * 1000);
-                connection.setConnectTimeout(5 * 1000);
-                connection.connect();
-                inputStream = connection.getInputStream();
-                StringBuilder sb = new StringBuilder();
-                byte[] buffer = new byte[4096];
-                int buffer_read = 0;
-                while (buffer_read != -1) {
-                    buffer_read = inputStream.read(buffer);
-                    if (buffer_read > 0) {
-                        sb.append(new String(buffer, 0, buffer_read));
+                if (body == null) {
+                    InputStream inputStream;
+                    HttpURLConnection connection;
+                    URL myURL;
+                    synchronized (lockObj) {
+                        myURL = new URL(url);
+                    }
+                    connection = (HttpURLConnection) myURL.openConnection();
+                    connection.setRequestMethod("GET");
+                    if (token != null) {
+                        connection.setRequestProperty("Authorization", "Token " + token);
+                    }
+                    connection.setReadTimeout(10 * 1000);
+                    connection.setConnectTimeout(5 * 1000);
+                    connection.connect();
+
+                    try {
+                        inputStream = connection.getInputStream();
+                    } catch (Exception e) {
+                        final BufferedReader bufferedReader = new BufferedReader(
+                                new InputStreamReader(connection.getErrorStream()));
+                        final StringBuilder errorString = new StringBuilder();
+                        String line;
+                        while ((line = bufferedReader.readLine()) != null) {
+                            errorString.append(line);
+                        }
+                        bufferedReader.close();
+                        System.out.println("ERROR STREAM: " + errorString.toString());
+                        throw new Exception(errorString.toString());
+                    }
+                    StringBuilder sb = new StringBuilder();
+                    byte[] buffer = new byte[4096];
+                    int buffer_read = 0;
+                    while (buffer_read != -1) {
+                        buffer_read = inputStream.read(buffer);
+                        if (buffer_read > 0) {
+                            sb.append(new String(buffer, 0, buffer_read));
+                        }
+                    }
+                    if (connection.getErrorStream() != null) {
+                        sb.append(connection.getResponseMessage());
+                    }
+                    remoteAPIDownloadCallback.displayData(sb.toString(), connection.getResponseCode(),
+                            connection.getResponseMessage());
+                    inputStream.close();
+                    connection.disconnect();
+                } else if (body.contentType().type().equals("multipart")) {
+                    ImageFileRequestBody imageFileRequestBody;
+                    Request request;
+
+                    OkHttpClient client = new OkHttpClient().newBuilder()
+                            .followRedirects(false)
+                            .followSslRedirects(false)
+                            .build();
+                    okhttp3.Response response = null;
+
+                    switch (body.contentType().type()) {
+                        case "multipart":
+                            imageFileRequestBody = new ImageFileRequestBody(body);
+                            synchronized (this) {
+                                request = new Request.Builder()
+                                        .header("Authorization", "Token " + token)
+                                        .url(url)
+                                        .post(imageFileRequestBody)
+                                        .build();
+                            }
+                            response = client.newCall(request).execute();
+                            break;
+                    }
+                    try {
+                        remoteAPIDownloadCallback.displayData(response.toString(), response.code(), response.message());
+                    } catch (Exception e) {
+                        e.printStackTrace();
                     }
                 }
-                remoteAPIDownloadCallback.displayData(sb.toString(), connection.getResponseCode(),
-                        connection.getResponseMessage());
-                inputStream.close();
-                connection.disconnect();
             } catch (Exception e) {
                 remoteAPIDownloadCallback.displayException(e);
             }
             synchronized (lockObj) {
-                remoteAPIDownloadCallback = null;
-                token = null;
-                url = null;
+                this.remoteAPIDownloadCallback = null;
+                this.body = null;
+                this.token = null;
+                this.url = null;
             }
         }
     }
