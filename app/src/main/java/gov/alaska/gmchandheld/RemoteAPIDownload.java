6b3701f32e21cfae7cc5d4211741ae9e84b478e6
Tommy Folan <tommy.folan@alaska.gov>
2021-10-07 09:01:55 -0800

    Remove HttpConnect from RemoteAPIDownload and replace it with OKHttp for all requests.

--- app/src/main/java/gov/alaska/gmchandheld/RemoteAPIDownload.java
+++ app/src/main/java/gov/alaska/gmchandheld/RemoteAPIDownload.java
@@ -1,8 +1,7 @@
 package gov.alaska.gmchandheld;
 
-import java.io.InputStream;
-import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.concurrent.TimeUnit;
 
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -10,14 +9,13 @@ import okhttp3.RequestBody;
 
 public class RemoteAPIDownload implements Runnable {
 
-    private final Object lockObj = new Object();
+    private final Object lockObj;
     private RemoteAPIDownloadCallback remoteAPIDownloadCallback;
     private String url, token;
     private RequestBody body;
-    private boolean running;
 
     public RemoteAPIDownload() {
-        running = true;
+        lockObj = new Object();
     }
 
     public void setUrl(String url) {
@@ -29,7 +27,6 @@ public class RemoteAPIDownload implements Runnable {
                                 RequestBody body,
                                 RemoteAPIDownloadCallback remoteAPIDownloadCallback) throws Exception {
 
-        running = true;
 
         if (url == null) {
             throw new Exception("URL is null");
@@ -45,7 +42,6 @@ public class RemoteAPIDownload implements Runnable {
         }
 
         synchronized (lockObj) {
-            System.out.print(" lock 1 ");
             if (this.url != null) {
                 throw new Exception("Uploading is busy");
             }
@@ -59,73 +55,45 @@ public class RemoteAPIDownload implements Runnable {
 
     public void run() {
         //Infinitely produce items
-        while (running) {
-            if (Thread.currentThread().isInterrupted()) {
-                running = false;
-            }
+        while (true) {
             synchronized (lockObj) {
-                System.out.print("************** lock 2 ");
                 try {
+                    url = null;
                     lockObj.wait();
                 } catch (Exception e) {
-                    System.out.println("Exception thrown in lock 2: " + e.getMessage());
                     continue;
                 }
             }
-
+            Request request;
+            okhttp3.Response response;
+            OkHttpClient client = new OkHttpClient.Builder()
+                    .followRedirects(false)
+                    .followSslRedirects(false)
+                    .connectTimeout(5, TimeUnit.SECONDS)
+                    .writeTimeout(5, TimeUnit.SECONDS)
+                    .readTimeout(5, TimeUnit.SECONDS)
+                    .build();
             try {
                 if (body == null) {
-                    InputStream inputStream;
-                    HttpURLConnection connection;
-                    URL myURL;
-                    synchronized (lockObj) {
-                        System.out.print(" lock 3 ");
-                        myURL = new URL(url);
-                    }
-                    connection = (HttpURLConnection) myURL.openConnection();
-                    connection.setRequestMethod("GET");
-                    if (token != null) {
-                        connection.setRequestProperty("Authorization", "Token " + token);
-                    }
-                    connection.setReadTimeout(5 * 1000);
-                    connection.setConnectTimeout(5 * 1000);
-                    connection.connect();
+                    try {
 
-                    System.out.print("Response code: " + url.toString() + " " + connection.getResponseCode());
-
-                    StringBuilder sb = new StringBuilder();
-                    inputStream = connection.getInputStream();
-                    byte[] buffer = new byte[4096];
-                    int buffer_read = 0;
-                    while (buffer_read != -1) {
-                        buffer_read = inputStream.read(buffer);
-                        if (buffer_read > 0) {
-                            sb.append(new String(buffer, 0, buffer_read));
+                        URL myURL;
+                        synchronized (lockObj) {
+                            myURL = new URL(url);
                         }
+                        request = new Request.Builder()
+                                .header("Authorization", "Token " + token)
+                                .url(myURL)
+                                .build();
+                        response = client.newCall(request).execute();
+                        remoteAPIDownloadCallback.displayData(response.body().string(), response.code(), response.message());
+                    } catch (Exception e) {
+                        e.printStackTrace();
                     }
-                    if (connection.getErrorStream() != null) {
-                        int length;
-                        while ((length = connection.getErrorStream().read(buffer)) != -1) {
-                            sb.append(new String(buffer, 0, length));
-                        }
-                        throw new Exception(sb.toString());
-                    }
-                    remoteAPIDownloadCallback.displayData(sb.toString(), connection.getResponseCode(),
-                            connection.getResponseMessage());
-                    inputStream.close();
-                    connection.disconnect();
                 } else if (body.contentType().type().equals("multipart")) {
                     ImageFileRequestBody imageFileRequestBody;
-                    Request request;
-
-                    OkHttpClient client = new OkHttpClient().newBuilder()
-                            .followRedirects(false)
-                            .followSslRedirects(false)
-                            .build();
-                    okhttp3.Response response;
                     imageFileRequestBody = new ImageFileRequestBody(body);
                     synchronized (this) {
-                        System.out.print(" lock 4 ");
                         request = new Request.Builder()
                                 .header("Authorization", "Token " + token)
                                 .url(url)
@@ -133,21 +101,10 @@ public class RemoteAPIDownload implements Runnable {
                                 .build();
                     }
                     response = client.newCall(request).execute();
-                    try {
-                        remoteAPIDownloadCallback.displayData(response.toString(), response.code(), response.message());
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
+                    remoteAPIDownloadCallback.displayData(response.toString(), response.code(), response.message());
                 }
             } catch (Exception e) {
                 remoteAPIDownloadCallback.displayException(e);
-            }
-            synchronized (lockObj) {
-                System.out.print(" lock 5 ");
-                this.remoteAPIDownloadCallback = null;
-                this.body = null;
-                this.token = null;
-                this.url = null;
             }
         }
     }
