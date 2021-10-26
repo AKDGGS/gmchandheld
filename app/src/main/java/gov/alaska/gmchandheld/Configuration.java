93cb7a1ca77cc3e1fa36a9a33a2b5894966b32e5
Tommy Folan <tommy.folan@alaska.gov>
2021-10-26 10:58:20 -0800

    Replace the hour and minute fields from Configuration with interval field.
    The alarm is triggered once every interval, before it was timed to the time of day.

--- app/src/main/java/gov/alaska/gmchandheld/Configuration.java
+++ app/src/main/java/gov/alaska/gmchandheld/Configuration.java
@@ -1,8 +1,5 @@
 package gov.alaska.gmchandheld;
 
-import android.app.AlarmManager;
-import android.app.PendingIntent;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
@@ -10,7 +7,6 @@ import android.os.StrictMode;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.KeyEvent;
-import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -26,14 +22,11 @@ import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
 import java.text.DateFormat;
-import java.util.Calendar;
 import java.util.Date;
-import java.util.Locale;
 
 public class Configuration extends BaseActivity implements RemoteAPIDownloadCallback {
     private ToggleButton autoUpdateBtn, cameraToScannerBtn;
-    private EditText hourInput, minuteInput, urlET;
-    private String hour, minute, url;
+    private EditText updateIntervalET, urlET;
 
     @Override
     public int getLayoutResource() {
@@ -83,8 +76,7 @@ public class Configuration extends BaseActivity implements RemoteAPIDownloadCall
         TextView buildDateTV = findViewById(R.id.buildDateTV);
         buildDateTV.setText(DateFormat.getDateTimeInstance().format(buildDate));
         autoUpdateBtn = findViewById(R.id.autoUpdateBtn);
-        hourInput = findViewById(R.id.hourET);
-        minuteInput = findViewById(R.id.minuteET);
+        updateIntervalET = findViewById(R.id.updateIntervalET);
         cameraToScannerBtn = findViewById(R.id.cameraToScannerBtn);
         Button urlCameraBtn = findViewById(R.id.urlCameraBtn);
         if (!sp.getBoolean("cameraOn", false)) {
@@ -95,20 +87,21 @@ public class Configuration extends BaseActivity implements RemoteAPIDownloadCall
         }
 
         urlCameraBtn.setOnClickListener(view -> {
-            checkSDKLevel();
+            if (Build.VERSION.SDK_INT <= 24) {
+                BaseActivity.intent = qrScan.createScanIntent();
+            } else {
+                BaseActivity.intent = new Intent(Configuration.this, CameraToScanner.class);
+            }
             startActivityForResult(BaseActivity.intent, 1);
         });
 
         final Button updateBtn = findViewById(R.id.updateBtn);
         updateBtn.setOnClickListener(v -> updateAPK());
         updateViews();
-        hourInputChangeWatcher();
-        minuteInputChangeWatcher();
         urlInputChangeWatcher();
-        autoUpdateChangeWatcher();
+        updateIntervalChangeFocusWatcher();
         cameraToScannerChangeWatcher();
         loadData();
-        saveData();
     }
 
     public void cameraToScannerChangeWatcher() {
@@ -117,60 +110,6 @@ public class Configuration extends BaseActivity implements RemoteAPIDownloadCall
                 BaseActivity.editor.putBoolean("cameraOn", true).apply();
             } else {
                 BaseActivity.editor.putBoolean("cameraOn", false).apply();
-            }
-        });
-    }
-
-    public void autoUpdateChangeWatcher() {
-        final Intent intent = new Intent(Configuration.this,
-                UpdateBroadcastReceiver.class);
-        boolean alarmUp = (PendingIntent.getBroadcast(Configuration.this, 2,
-                intent, 0) != null);
-        autoUpdateBtn.setChecked(alarmUp);
-        autoUpdateBtn.setOnCheckedChangeListener((compoundButton, isChecked) -> {
-            if (isChecked) {
-                PendingIntent sender = PendingIntent.getBroadcast(
-                        Configuration.this, 2, intent, 0);
-                AlarmManager am = (AlarmManager) Configuration.this
-                        .getSystemService(Context.ALARM_SERVICE);
-                if (am != null) {
-                    hour = BaseActivity.sp.getString("updateHour", "24");
-                    minute = BaseActivity.sp.getString("updateMinute", "0");
-                    Calendar alarmOffTime = Calendar.getInstance();
-                    if (!hour.isEmpty()) {
-                        alarmOffTime.set(Calendar.HOUR_OF_DAY, Integer.parseInt(hour));
-                    } else {
-                        alarmOffTime.set(Calendar.HOUR_OF_DAY, 24);
-                        hourInput = findViewById(R.id.hourET);
-                        hourInput.setText(R.string._24);
-                    }
-                    if (!minute.isEmpty()) {
-                        alarmOffTime.set(Calendar.MINUTE, Integer.parseInt(minute));
-                    } else {
-                        alarmOffTime.set(Calendar.MINUTE, 0);
-                        minuteInput = findViewById(R.id.minuteET);
-                        minuteInput.setText(R.string._0);
-                    }
-                    alarmOffTime.set(Calendar.SECOND, 0);
-                    if (alarmOffTime.before(Calendar.getInstance())) {
-                        alarmOffTime.add(Calendar.DATE, 1);
-                    }
-                    BaseActivity.editor.putBoolean("alarmOn", true);
-                    am.setRepeating(AlarmManager.RTC_WAKEUP,
-                            alarmOffTime.getTimeInMillis(),
-                            AlarmManager.INTERVAL_DAY, sender);
-                }
-            } else {
-                Intent intent1 = new Intent(Configuration.this,
-                        UpdateBroadcastReceiver.class);
-                PendingIntent sender = PendingIntent.getBroadcast(
-                        Configuration.this, 2, intent1, 0);
-                AlarmManager am = (AlarmManager) Configuration.this
-                        .getSystemService(Context.ALARM_SERVICE);
-                if (am != null) {
-                    am.cancel(sender);
-                }
-                BaseActivity.editor.putBoolean("alarmOn", true);
             }
         });
     }
@@ -189,15 +128,11 @@ public class Configuration extends BaseActivity implements RemoteAPIDownloadCall
             @Override
             public void afterTextChanged(Editable s) { }
         });
-        if (sp.getBoolean("firstrun", true)) {
-            checkIssuesList();
-            sp.edit().putBoolean("firstrun", false).apply();
-        }
     }
 
     public String getUrl() {
         urlET = findViewById(R.id.urlET);
-        url = urlET.getText().toString();
+        String url = urlET.getText().toString();
         if (url.length() > 1 && url.charAt(url.length() - 1) != ('/')) {
             url = url + '/';
         }
@@ -206,111 +141,61 @@ public class Configuration extends BaseActivity implements RemoteAPIDownloadCall
 
     public void saveData() {
         BaseActivity.editor.putString("urlText", getUrl());
-        BaseActivity.editor.putString("updateHour", hourInput.getText().toString());
-        BaseActivity.editor.putString("updateMinute", minuteInput.getText().toString());
+        BaseActivity.editor.putString("interval", updateIntervalET.getText().toString());
         BaseActivity.editor.putBoolean("cameraOn", cameraToScannerBtn.isChecked());
         BaseActivity.editor.putBoolean("alarmOn", autoUpdateBtn.isChecked());
         BaseActivity.editor.apply();
-        checkIssuesList();
+        if (sp.getString("urlText", "").isEmpty()) {
+            Intent intent = new Intent(this, GetToken.class);
+            startActivityForResult(intent, 0);
+        }
     }
 
     public void loadData() {
-        url = sp.getString("urlText", "");
-        hour = sp.getString("updateHour", "24");
-        minute = sp.getString("updateMinute", "0");
+        urlET.setText(sp.getString("urlText", ""));
+        updateIntervalET.setText(sp.getString("interval", "60"));
         autoUpdateBtn.setChecked(sp.getBoolean("alarmOn", true));
         cameraToScannerBtn.setChecked(sp.getBoolean("cameraOn", false));
     }
 
     public void updateViews() {
         urlET.setText(BaseActivity.sp.getString("urlText", ""));
-        hourInput.setText(sp.getString("updateHour", "24"));
-        minuteInput.setText(sp.getString("updateMinute", "0"));
+        updateIntervalET.setText(sp.getString("interval", "60"));
         autoUpdateBtn.setChecked(sp.getBoolean("alarmOn", true));
         cameraToScannerBtn.setChecked(sp.getBoolean("cameraOn", false));
     }
 
-    public void hourInputChangeWatcher() {
-        hourInput.addTextChangedListener(new TextWatcher() {
-            String strBefore;
-
+    public void updateIntervalChangeFocusWatcher() {
+        updateIntervalET = findViewById(R.id.updateIntervalET);
+        updateIntervalET.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                if (s.length() != 0) {
-                    limit(hourInput, 24);
-                    if (hourInput.getText().toString().equals("24")) {
-                        minuteInput.setText("0");
-                        BaseActivity.editor.putString("updateMinute", minuteInput.getText()
-                                .toString()).apply();
-                    }
-                    BaseActivity.editor.putString("updateHour", hourInput.getText()
-                            .toString()).apply();
-                } else {
-                    BaseActivity.editor.putString("updateHour", "24").apply();
-                    minuteInput.setText("0");
-                    BaseActivity.editor.putString("updateMinute", minuteInput.getText()
-                            .toString()).apply();
-                }
-            }
-
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-                strBefore = s.toString();
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-                if (!s.toString().equals(strBefore)) {
-                    autoUpdateBtn.setChecked(false);
-                    autoUpdateBtn.setChecked(true);
-                }
-            }
-        });
-    }
-
-    public void minuteInputChangeWatcher() {
-        minuteInput.addTextChangedListener(new TextWatcher() {
-            String strBefore;
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                if (s.length() != 0 && !(hourInput.getText().toString().equals("24"))) {
-                    limit(minuteInput, 59);
-                    BaseActivity.editor.putString("updateMinute", s.toString()).apply();
-                } else {
-                    BaseActivity.editor.putString("updateMinute", "0").apply();
-                    limit(minuteInput, 0);
-                }
-            }
-
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-                strBefore = s.toString();
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-                if (!s.toString().equals(strBefore)) {
-                    autoUpdateBtn.setChecked(false);
-                    autoUpdateBtn.setChecked(true);
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (!hasFocus) {
+                    cancelAlarm();
+                    setAlarm();
                 }
             }
         });
     }
 
     public void updateAPK() {
-        if (!urlET.getText().toString().isEmpty()) {
-            checkIssuesList();
-            new UpdateCheckLastModifiedDate(this).execute();
+        if (BaseActivity.getUpdateAvailable()) {
+            Intent intent = new Intent(Configuration.this, UpdateDownloadAPKHandler.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            Configuration.this.startActivity(intent);
+        } else {
+            Toast.makeText(Configuration.this, "No update is available.",
+                    Toast.LENGTH_SHORT).show();
         }
     }
 
     public void checkIssuesList() {
         try {
             remoteAPIDownload.setFetchDataObj("https://maps.dggs.alaska.gov/gmcdev/qualitylist.json",
-                    BaseActivity.apiKeyBase,
+                    apiKeyBase,
                     null,
-                    this);
+                    this,
+                    0);
         } catch (Exception e) {
             Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_LONG).show();
         }
@@ -334,43 +219,31 @@ public class Configuration extends BaseActivity implements RemoteAPIDownloadCall
         }
     }
 
-    public void limit(EditText x, int limax) {
-//      https://stackoverflow.com/a/25366712
-        if (x.getText().toString() == null || x.getText().toString().length() == 0) {
-            x.setText(String.format(Locale.getDefault(), "%d", 0));
-        } else {
-            int z = Integer.parseInt(x.getText().toString());
-            if (z < 0 || z > limax) {
-                if (z < 0) {
-                    x.setText(String.format(Locale.getDefault(), "%d", 0));
-                } else {
-                    x.setText(String.format(Locale.getDefault(), "%d", limax));
-                }
-            }
-        }
-    }
-
-    public void checkSDKLevel() {
-        if (Build.VERSION.SDK_INT <= 24) {
-            BaseActivity.intent = qrScan.createScanIntent();
-        } else {
-            BaseActivity.intent = new Intent(Configuration.this, CameraToScanner.class);
-        }
-    }
-
     @Override
-    public boolean onPrepareOptionsMenu(Menu menu) {
-        if (!urlET.getText().toString().startsWith("https")) {
-            menu.clear();
-        }
-        return super.onPrepareOptionsMenu(menu);
-    }
-
-    @Override
-    public void displayData(String data, int responseCode, String responseMessage) {
+    public void displayData(String data, int responseCode, String responseMessage, int requestType) {
         if (responseCode != 403) {
-            editor = sp.edit();
-            editor.putString("issuesString", data).commit();
+            switch (requestType) {
+                case RemoteAPIDownload.GET:
+                    Date updateBuildDate = new Date(data);
+                    Date buildDate = new Date(BuildConfig.TIMESTAMP);
+
+                    //gets the last refused modified date from shared preferences.
+                    // (The last refused modified date comes from UpdateDownloadAPKHandler
+                    long lastRefusedUpdate = BaseActivity.sp.getLong("ignoreUpdateDateSP", 0);
+
+                    BaseActivity.updateAvailable = !(updateBuildDate.compareTo(
+                            new Date(lastRefusedUpdate)) == 0) &
+                            (buildDate.compareTo(updateBuildDate) < 0);
+                    checkIssuesList();
+                    break;
+                case RemoteAPIDownload.HEAD:
+                    editor = sp.edit();
+                    editor.putString("issuesString", data).commit();
+                    System.out.println(data);
+                    break;
+                default:
+                    System.out.println("Error: the request Type isn't understood");
+            }
         } else {
             Toast.makeText(Configuration.this,
                     "The token is not correct.", Toast.LENGTH_LONG).show();
@@ -384,6 +257,7 @@ public class Configuration extends BaseActivity implements RemoteAPIDownloadCall
                 @Override
                 public void run() {
                     Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_LONG).show();
+                    e.printStackTrace();
                 }
             });
         }
