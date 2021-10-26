7fbe257d9ca5249de6de6607efd6d8fb64f1b259
Tommy Folan <tommy.folan@alaska.gov>
2021-10-26 11:00:15 -0800

    Add requestType to RemoteAPIDownloadCallback displayData signature.

--- app/src/main/java/gov/alaska/gmchandheld/RemoteAPIDownloadCallback.java
+++ app/src/main/java/gov/alaska/gmchandheld/RemoteAPIDownloadCallback.java
@@ -1,7 +1,10 @@
 package gov.alaska.gmchandheld;
 
 public interface RemoteAPIDownloadCallback {
-    void displayData(String data, int responseCode, String responseMessage);
+    void displayData(String data,
+                     int responseCode,
+                     String responseMessage,
+                     int requestType);
 
     void displayException(Exception e);
 }
